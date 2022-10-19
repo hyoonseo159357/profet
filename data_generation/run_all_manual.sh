e9c69bc7b1c29acdbe9df3a999dd45c0e9a0663b
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-20 00:42:38 +0900

    Update run_all_manual.sh

--- data_generation/run_all_manual.sh
+++ data_generation/run_all_manual.sh
@@ -1,4 +1,12 @@
-for model in VGGSmall LeNet5
+for model in VGGSmall LeNet5 VGG11 VGG13 VGG16 VGG19 ResNetSmall ResNet18 ResNet34 ResNet50 MNIST_CNN CIFAR10_CNN FLOWER_CNN AlexNet InceptionV3 InceptionResNetV2 Xception EfficientNetB0 MobileNetV2
 do
-  sudo python3 /home/ubuntu/profet/data_generation/workload/profile_workload.py --model $model
+  for dataset in  32 64 128 224 256
+  do
+    for batchsize in 16 32 64 128 256 
+    do
+      for prof_or_latency in profiling latency
+        sudo python3 /home/ubuntu/profet/data_generation/workload/profile_workload.py --model $model --dataset $dataset
+      done
+    done
+  done
 done
