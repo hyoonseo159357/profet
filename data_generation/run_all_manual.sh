8b624a174bfa007403eefb0b835b9ce5a5d59ded
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-20 00:12:50 +0900

    Update run_all_manual.sh

--- data_generation/run_all_manual.sh
+++ data_generation/run_all_manual.sh
@@ -1,4 +1,4 @@
-for model in ['VGGSmall','LeNet5']
+for model in VGGSmall LeNet5
 do
-  python3 /home/ubuntu/profet/data_generation/workload/profile_workload.py --model $model
+  sudo python3 /home/ubuntu/profet/data_generation/workload/profile_workload.py --model $model
 done
