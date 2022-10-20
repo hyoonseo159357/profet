2ef86af47bfdfb009a7a3470767cf691e69894c9
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-20 16:11:15 +0900

    Update settings.sh

--- data_generation/settings.sh
+++ data_generation/settings.sh
@@ -8,3 +8,10 @@ sudo apt-get install python3-pip   # pip3 이 없어서 설치해줘야한다.
 pip3 install -r requirements.txt
 mkdir ./logs
 mkdir ./tensorstats
+sudo apt update
+sudo killall apt apt-get
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
+sudo dpkg --configure -a
+sudo apt update
