2ace179adad0c19530b21443d2e531625757574c
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-20 16:15:57 +0900

    Update settings.sh

--- data_generation/settings.sh
+++ data_generation/settings.sh
@@ -7,6 +7,8 @@ sudo apt-get update
 sudo apt-get install python3-pip   # pip3 이 없어서 설치해줘야한다.
 
 sudo apt-get install --reinstall python3-apt
+sudo apt-get remove python3-apt
+sudo apt-get install python3-apt
 
 pip3 install -r requirements.txt
 mkdir ./logs
