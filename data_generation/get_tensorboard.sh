f75ef3999b59e81e33bffa4d7e840c7068783dc4
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-20 13:58:38 +0900

    Update get_tensorboard.sh

--- data_generation/get_tensorboard.sh
+++ data_generation/get_tensorboard.sh
@@ -5,7 +5,7 @@ sleep 10
 NAME_LIST=$(find ./logs -name *.trace.json.gz)
 
 TB_URL1="http://localhost:6006/data/plugin/profile/data?run="
-TB_URL3="/train/"
+#TB_URL3="/train/"
 TB_URL5="&tag=tensorflow_stats&host="
 TB_URL7="&tqx=out:csv;"
 
@@ -14,7 +14,8 @@ do
         TB_URL2=$(echo $NAME | cut -d "/" -f 3)
         TB_URL4=$(echo $NAME | cut -d "/" -f 7)
         TB_URL6=$(echo $NAME | cut -d "/" -f 8 | cut -d "." -f 1)
-        TB_URL="$TB_URL1$TB_URL2$TB_URL3$TB_URL4$TB_URL5$TB_URL6$TB_URL7"
+        #TB_URL="$TB_URL1$TB_URL2$TB_URL3$TB_URL4$TB_URL5$TB_URL6$TB_URL7"
+        TB_URL="$TB_URL1$TB_URL2$TB_URL4$TB_URL5$TB_URL6$TB_URL7"
         echo $TB_URL
         FILENAME="./tensorstats/$TB_URL2$TB_URL4.csv"
         curl -o $FILENAME $TB_URL
