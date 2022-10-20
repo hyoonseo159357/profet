dee191790162391513384ef4b9c1b3a804aa2ac8
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-20 13:04:31 +0900

    Update run_single_manul.sh

--- data_generation/run_single_manul.sh
+++ data_generation/run_single_manul.sh
@@ -1,4 +1,4 @@
-for model in VGGSmall LeNet5 VGG11 VGG13 VGG16
+for model in VGGSmall LeNet5
 do
   sudo python3 /home/ubuntu/profet/data_generation/workload/profile_workload.py --model $model
 done
