5cc0f238213f7fa94bbae2ea23bf62b435e9f361
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-24 15:18:02 +0900

    Update settings.sh

--- data_generation/settings.sh
+++ data_generation/settings.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update
+sudo apt install python3.7 # 설치하려는 버전 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
 sudo apt-get remove  python3-apt
@@ -6,9 +8,8 @@ sudo apt-get install python3-apt
 sudo apt-get update 
 sudo apt-get install python3-pip   # pip3 이 없어서 설치해줘야한다.
 
-sudo apt-get install --reinstall python3-apt
-sudo apt-get remove python3-apt
-sudo apt-get install python3-apt
+pip3 install --upgrade setuptools
+sudo -H pip3 install --upgrade pip
 
 pip3 install -r requirements.txt
 mkdir ./logs
