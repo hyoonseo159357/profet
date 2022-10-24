f9f1a3b9eff6b5cd033b77e97e225b61b2de6bb8
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-10-24 14:54:04 +0900

    Update run_single_manul.sh

--- data_generation/run_single_manul.sh
+++ data_generation/run_single_manul.sh
@@ -1,7 +1,7 @@
 # warm start 용도
 for model in LeNet5
 do
-  sudo python3 /home/kmubigdatagcp/profet/data_generation/workload/profile_workload.py --model $model
+  sudo python3 /root/profet/data_generation/workload/profile_workload.py --model $model
 done
 sudo rm -r logs
 mkdir logs
