d81d9d7d5facc9f30a218b29f3f5bed70b4df6e5
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2023-01-06 16:34:43 +0900

    Update run_single_gcp.sh

--- data_generation/run_single_gcp.sh
+++ data_generation/run_single_gcp.sh
@@ -1,1 +1,13 @@
-run_all_manual_gcp.sh
+or model in ResNet18
+do
+  for dataset in  32
+  do
+    for batchsize in 16
+    do
+      for prof_or_latency in profiling latency
+      do
+        sudo python3 /home/kmubigdatagcp/profet/data_generation/workload/profile_workload5.py --model $model --dataset $dataset --batch_size $batchsize --prof_or_latency $prof_or_latency
+      done
+    done
+  done
+done
